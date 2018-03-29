eff58386c85466076a391273e36e434a206711be
tamerlan88111 <tamerlan88111@yandex.ru>
2018-03-29 12:02:56 +0300

    show screens and handle switches them

---
+++ js/main.js
@@ -0,0 +1,53 @@
+(function () {
+  const KEYS = {
+    ALT: 18,
+    LEFT: 37,
+    RIGHT: 39
+  };
+  const template = document.getElementById(`templates`);
+  const screens = template && template.content.querySelectorAll(`.main`);
+  const main = document.querySelector(`.main`);
+  let isAltPress = false;
+  let currentScreen = 0;
+
+  const getScreen = (i) => screens[i] || screens[currentScreen];
+  const showScreen = (number) => {
+    if (number > screens.length - 1 || number < 0) {
+      return;
+    }
+    const screen = getScreen(number);
+    main.innerHTML = screen.outerHTML;
+    currentScreen = number;
+  };
+
+  const onKeyUpHandler = (evt) => {
+    if (evt.keyCode === KEYS.ALT) {
+      isAltPress = false;
+      document.addEventListener(`keyup`, onKeyUpHandler);
+    }
+  };
+
+  const onKeyDownHandler = (evt) => {
+    switch (evt.keyCode) {
+      case KEYS.ALT:
+        isAltPress = true;
+        document.addEventListener(`keyup`, onKeyUpHandler);
+        break;
+      case KEYS.RIGHT:
+        if (isAltPress) {
+          showScreen(currentScreen + 1);
+        }
+        break;
+      case KEYS.LEFT:
+        if (isAltPress) {
+          showScreen(currentScreen - 1);
+        }
+        break;
+    }
+  };
+
+  document.addEventListener(`keydown`, onKeyDownHandler);
+
+  showScreen(currentScreen);
+})();
+
