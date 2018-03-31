ffd07e54b3aee0ed5559bb6c01ef32388a2d298f
rezunenko <tamerlan88111@yandex.ru>
2018-03-31 23:40:52 +0300

    add getElementFromTemplate

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
-(function () {
-  const KEYS = {
-    LEFT: 37,
-    RIGHT: 39
-  };
-  const template = document.getElementById(`templates`);
-  const screens = template && template.content.querySelectorAll(`.main`);
-  const main = document.querySelector(`.main`);
-  let currentScreen = 0;
+import {getElementFromTemplate} from './utils';
 
-  const getScreen = (i) => screens[i] || screens[currentScreen];
-  const showScreen = (number) => {
-    if (number > screens.length - 1 || number < 0) {
-      return;
-    }
-    const screen = getScreen(number);
-    main.innerHTML = screen.outerHTML;
-    currentScreen = number;
-  };
+const KEYS = {
+  LEFT: 37,
+  RIGHT: 39
+};
+const template = document.getElementById(`templates`);
+const screens = template && template.content.querySelectorAll(`.main`);
+const main = document.querySelector(`.main`);
+let currentScreen = 0;
 
-  const onKeyUpHandler = (evt) => {
-    if (evt.keyCode === KEYS.RIGHT && evt.altKey) {
-      showScreen(currentScreen + 1);
-    }
+const getScreen = (i) => screens[i] || screens[currentScreen];
+const showScreen = (number) => {
+  if (number > screens.length - 1 || number < 0) {
+    return;
+  }
+  const screen = getScreen(number);
+  main.innerHTML = screen.outerHTML;
+  currentScreen = number;
+};
 
-    if (evt.keyCode === KEYS.LEFT && evt.altKey) {
-      showScreen(currentScreen - 1);
-    }
-  };
+const onKeyUpHandler = (evt) => {
+  if (evt.keyCode === KEYS.RIGHT && evt.altKey) {
+    showScreen(currentScreen + 1);
+  }
 
-  document.addEventListener(`keyup`, onKeyUpHandler);
+  if (evt.keyCode === KEYS.LEFT && evt.altKey) {
+    showScreen(currentScreen - 1);
+  }
+};
 
-  showScreen(currentScreen);
-})();
+document.addEventListener(`keyup`, onKeyUpHandler);
+
+showScreen(currentScreen);
+
 
---
+++ js/utils.js
@@ -0,0 +1,12 @@
+export const getElementFromTemplate = (str) => {
+  const element = document.createElement('div');
+  element.innerHTML = str;
+
+  const fragment = [...element.children].reduce((res, child) => {
+    res.appendChild(child);
+
+    return res;
+  }, document.createDocumentFragment());
+
+  return fragment;
+};
