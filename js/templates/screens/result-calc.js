68a448ae14cd0e7e647082f166973ad2a079c738
rezunenko <tamerlan88111@yandex.ru>
2018-04-05 00:14:03 +0300

    show result funcion and tests

--- js/templates/screens/result-calc.js
+++ js/templates/screens/result-calc.js
@@ -9,13 +9,6 @@ const getPoints = ({ isCorrect, time }) => {
   return 1;
 };
 
-const res = {
-  userId: 1,
-  points: 14,
-  remainNotes: 2,
-  remainTimes: 22
-};
-
 export const calculateResult = (answers, remainNotes = 0) => {
   if (!remainNotes) {
     return 0;
@@ -35,9 +28,17 @@ export const calculateResult = (answers, remainNotes = 0) => {
   return (answers || []).reduce((res, answer) => res + getPoints(answer), 0);
 };
 
-export const showUserResult = (total, result) => {
-  const userResult = {result, currentUser: true};
-  const all = [...results, userResult].sort((a, b) => a.points > b.points);
+export const showUserResult = (total = res, result) => {
+  const userResult = Object.assign({}, result, {currentUser: true});
+  const all = [...total, userResult].sort((a, b) => a.points < b.points);
   const pos = all.findIndex(item => item.currentUser) + 1;
-  const persent = Math.round((pos / all.length) * 100);
+  const persent = Math.round(((all.length - pos) / all.length) * 100);
+
+  if (result.points > 0) {
+    return `Вы заняли ${pos}-ое место из ${all.length}. Это лучше чем у ${persent}% игроков.`;
+  } else if (result.remainTimes === 0) {
+    return `Время вышло! Вы не успели отгадать все мелодии`;
+  }
+
+  return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`
 };
--- js/templates/screens/result-calc.test.js
+++ js/templates/screens/result-calc.test.js
@@ -17,6 +17,34 @@ const TEST_ANSWERS = {
   }
 };
 
+const STATISTICS = [
+  {
+    points: 25,
+    remainNotes: 2,
+    remainTimes: 22
+  },
+  {
+    points: 14,
+    remainNotes: 2,
+    remainTimes: 22
+  },
+  {
+    points: 1,
+    remainNotes: 2,
+    remainTimes: 22
+  },
+  {
+    points: 4,
+    remainNotes: 2,
+    remainTimes: 22
+  },
+  {
+    points: 6,
+    remainNotes: 2,
+    remainTimes: 22
+  },
+];
+
 describe(`Result page`, () => {
   describe(`Calculate result`, () => {
     it(`Minimum correct answers is 10`, () => {
@@ -44,11 +72,49 @@ describe(`Result page`, () => {
     });
   });
 
-  // describe(`Show result`, () => {
-  //   it(`Correct answer return 1 point`, () => {
-  //     assert.equal(showUserResult([], ), `Время вышло! Вы не успели отгадать все мелодии`);
-  //   });
+  describe(`Show result`, () => {
+    it(`correct position and persentage info`, () => {
+      assert.equal(showUserResult(STATISTICS, {
+        points: 8,
+        remainNotes: 2,
+        remainTimes: 22
+      }), `Вы заняли 3-ое место из 6. Это лучше чем у 50% игроков.`);
 
+      assert.equal(showUserResult(STATISTICS, {
+        points: 100,
+        remainNotes: 2,
+        remainTimes: 22
+      }), `Вы заняли 1-ое место из 6. Это лучше чем у 83% игроков.`);
 
-  // });
+      assert.equal(showUserResult(STATISTICS, {
+        points: 1,
+        remainNotes: 2,
+        remainTimes: 22
+      }), `Вы заняли 6-ое место из 6. Это лучше чем у 0% игроков.`);
+    });
+
+    it(`get position and persentage if statistics are empty`, () => {
+      assert.equal(showUserResult([], {
+        points: 8,
+        remainNotes: 2,
+        remainTimes: 22
+      }), `Вы заняли 1-ое место из 1. Это лучше чем у 0% игроков.`);
+    });
+
+    it(`if notes ended then you lose`, () => {
+      assert.equal(showUserResult([], {
+        points: 0,
+        remainNotes: 0,
+        remainTimes: 22
+      }), `У вас закончились все попытки. Ничего, повезёт в следующий раз!`);
+    });
+
+    it(`if time ended then you lose`, () => {
+      assert.equal(showUserResult([], {
+        points: 0,
+        remainNotes: 2,
+        remainTimes: 0
+      }), `Время вышло! Вы не успели отгадать все мелодии`);
+    });
+  });
 });
