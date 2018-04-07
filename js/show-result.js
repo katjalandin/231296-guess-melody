bb20297cfb0308c89e4164762f6f3e4fb5734025
rezunenko <tamerlan88111@yandex.ru>
2018-04-07 13:51:15 +0300

    fix sorf result bug

--- js/show-result.js
+++ js/show-result.js
@@ -1,6 +1,6 @@
 export default (total, result) => {
   const userResult = Object.assign({}, result, {currentUser: true});
-  const all = [...total, userResult].sort((a, b) => a.points < b.points);
+  const all = [...total, userResult].sort((a, b) => b.points - a.points);
   const pos = all.findIndex((item) => item.currentUser) + 1;
   const persent = Math.round(((all.length - pos) / all.length) * 100);
 
